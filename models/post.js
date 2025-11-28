004205dbfc9abc38eca2b864cef1e2fec418c6d4
MarieAnne-VT <116596739+MarieAnne-VT@users.noreply.github.com>
2025-11-28 09:29:01 -0500

    first commit

---
+++ models/post.js
@@ -0,0 +1,18 @@
+import Model from './model.js';
+
+export default class Post extends Model {
+    constructor() {
+        super(true /* secured Id */);
+
+        this.addField('Title', 'string');
+        this.addField('Text', 'string');
+        this.addField('Category', 'string');
+        this.addField('Image', 'asset');
+        this.addField('Date', 'integer');
+
+        this.setKey("Title");
+    }
+
+    // Ajouter bind et join 
+    // this.bind or join (extends de Model)
+}
